4d8be5f7003478a3f3f0e3252a5bb5d3fb56c6a0
世纪末的架构师 <1072876976@qq.com>
2022-09-04 21:55:57 +0800

    fix(项目后端配置与SQL文件): 修改数据库连接信息
    1. 修改数据库名称中的-为_
    2. 修改项目配置文件参数
    3. sql文件中添加语句自动创建数据库

--- system-mysql8.sql
+++ system-mysql8.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 80013
  Source Host           : localhost:3306
- Source Schema         : system-template
+ Source Schema         : system_template
 
  Target Server Type    : MySQL
  Target Server Version : 80013
@@ -13,10 +13,11 @@
 
  Date: 04/09/2022 17:38:23
 */
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE system_template;
+USE system_template;
 -- ----------------------------
 -- Table structure for menu
 -- ----------------------------
